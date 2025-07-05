e7b334fe0ae57b6aa462ce71485e054d4c100805
AungNaingPhyo2006 <115768795+AungNaingPhyo2006@users.noreply.github.com>
2025-07-05 19:52:05 +0630

    add flutter game

---
+++ lib1/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_shop/bottomNavigation/main_navigator.dart';
+// import 'package:my_shop/home/home_page.dart';
+// import 'package:my_shop/screens/scanner/scanner_screen.dart';
+
+void main() {
+   runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      // home: const ScannerScreen(),
+      home: const MainNavigator()
+    );
+  }
+}
